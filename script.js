1f27d413c5679226c8e0e3be8fb8ff622561ac74
STORMTR00P3R <noorblum16@gmail.com>
2024-10-23 16:05:25 -0700

    push

---
+++ script.js
@@ -0,0 +1,8 @@
+// var col = document.createElement("div");
+// col.classList.add("col");
+// console.log(col)
+// document.body.appendChild(col);
+
+// // for (let i = 0; i < bar.length; i++) { 
+// //     text += bar[i] + "<br>";
+// // }
